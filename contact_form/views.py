0a30db15ed62b3511d2e5fe03995067aab5d7013
Lautaro <lautajamdev@gmail.com>
2023-07-07 21:40:45 -0300

    Now there is the form (HTML only) and the template

--- contact_form/views.py
+++ contact_form/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
 
 # Create your views here.
 def contact_form(request):
     return render(request, 'contact_form.html')
+
+def contact(request):
+    pass
